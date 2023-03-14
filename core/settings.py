871599997ada5224cfc45e9d473de13fa727d1b2
Daniel <yourmediasoft@gmail.com>
2023-03-14 14:02:41 -0500

    We create the build folder to make the connection with react and djangorestframework

--- core/settings.py
+++ core/settings.py
@@ -112,7 +112,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
