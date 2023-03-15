4918ca4cf0b8926a294253c9632a0ca4054e4d77
Daniel <yourmediasoft@gmail.com>
2023-03-15 09:45:09 -0500

    Configure static files and collectstatic management command

--- core/settings.py
+++ core/settings.py
@@ -215,7 +215,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+#Congigure Static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
