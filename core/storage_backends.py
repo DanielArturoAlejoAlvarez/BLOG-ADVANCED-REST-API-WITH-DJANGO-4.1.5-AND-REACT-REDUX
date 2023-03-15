0617ebf8bcdce303d737506bf83e6cefc10c50b4
Daniel <yourmediasoft@gmail.com>
2023-03-15 10:14:12 -0500

    Creating storage_backends file for AWS connection with Django

---
+++ core/storage_backends.py
@@ -0,0 +1,12 @@
+#AWS connection with Django
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticStorage(S3Boto3Storage):
+    location = 'static'
+    default_acl = 'private'
+
+
+class MediaStore(S3Boto3Storage):
+    location = 'media'
+    file_overwrite = False
