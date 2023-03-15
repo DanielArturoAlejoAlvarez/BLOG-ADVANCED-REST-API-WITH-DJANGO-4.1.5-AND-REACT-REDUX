0c61e5c00a128f239ccc3e7fc99c0f4193c678ed
Daniel <yourmediasoft@gmail.com>
2023-03-15 10:23:33 -0500

    Configure urls file in core system

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path,include
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html'))
 ]
