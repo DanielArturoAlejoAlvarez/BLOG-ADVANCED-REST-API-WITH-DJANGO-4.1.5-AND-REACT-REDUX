ac37bbb98093e88235fdd37eb933167735b3f2d0
Daniel <yourmediasoft@gmail.com>
2023-03-15 09:54:46 -0500

    Configure DjangoRestFramework giving the corresponding permissions

--- core/settings.py
+++ core/settings.py
@@ -231,3 +231,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Configure DJANGO REST FRAMEWORK
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 16,
+}
