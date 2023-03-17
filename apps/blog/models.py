1771b5e80596adfdae10113ab5e5325c5924c366
Daniel <yourmediasoft@gmail.com>
2023-03-16 19:44:01 -0500

    Refactoring Post model add relationship with Category model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 from django.utils import timezone
+from apps.category.models import Category
 import uuid
+
 
 
 def blog_directory_path(instance,filename):
@@ -14,36 +16,40 @@ class Post(models.Model):
         def get_queryset(self):
             return super().get_queryset().filter(status='published')
         
-        options=(
-            ('draft','Draft'),
-            ('piblished','Published')
-        )
+    options=(
+        ('draft','Draft'),
+        ('piblished','Published')
+    )
 
 
 
-        blog_uuid=models.UUIDField(default=uuid.uuid4,unique=True)
-        title=models.CharField(max_length=255)
-        slug=models.SlugField(max_length=255,unique=True)
-        thumbnail=models.ImageField(upload_to=blog_directory_path,max_length=512)
-        video=models.FileField(upload_to=blog_directory_path,blank=True,null=True)
-        content=models.TextField()
-        excerpt=models.CharField(max_length=100)
+    blog_uuid=models.UUIDField(default=uuid.uuid4,unique=True)
+    title=models.CharField(max_length=255)
+    slug=models.SlugField(max_length=255,unique=True)
+    thumbnail=models.ImageField(upload_to=blog_directory_path,max_length=512)
+    video=models.FileField(upload_to=blog_directory_path,blank=True,null=True)
+    content=models.TextField()
+    excerpt=models.CharField(max_length=100)
 
-        published=models.DateTimeField(default=timezone.now)
-        status=models.CharField(max_length=10, choices=options, default='draft')
+    category=models.ForeignKey(Category, on_delete=models.PROTECT)
 
+    published=models.DateTimeField(default=timezone.now)
+    status=models.CharField(max_length=10, choices=options, default='draft')
 
-        class Meta:
-            oredering=('-published',)
+    objects=models.Manager()
+    postobjects=PostObjects()
 
-        def __str__(self):
-            return self.title
+    class Meta:
+        oredering=('-published',)
+
+    def __str__(self):
+        return self.title
+    
+    def get_video(self):
+        if self.video:
+            return self.video.url
         
-        def get_video(self):
-            if self.video:
-                return self.video.url
-            
-        def get_thumbnail(self):
-            if self.thumbnail:
-                return self.thumbnail.url
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return self.thumbnail.url
 
