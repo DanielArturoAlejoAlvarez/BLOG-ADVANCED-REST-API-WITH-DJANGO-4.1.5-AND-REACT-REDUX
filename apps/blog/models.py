f4f35dc30637377f8a4d463ad29cc126e3db111c
Daniel <yourmediasoft@gmail.com>
2023-03-16 19:16:56 -0500

    Configure Post model and PostObjects Manager

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+import uuid
+
+
+def blog_directory_path(instance,filename):
+    return 'blog/{0}/{1}' .format(instance.title,filename)
+
+
+class Post(models.Model):
+
+    class PostObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status='published')
+        
+        options=(
+            ('draft','Draft'),
+            ('piblished','Published')
+        )
+
+
+
+        blog_uuid=models.UUIDField(default=uuid.uuid4,unique=True)
+        title=models.CharField(max_length=255)
+        slug=models.SlugField(max_length=255,unique=True)
+        thumbnail=models.ImageField(upload_to=blog_directory_path,max_length=512)
+        video=models.FileField(upload_to=blog_directory_path,blank=True,null=True)
+        content=models.TextField()
+        excerpt=models.CharField(max_length=100)
+
+        published=models.DateTimeField(default=timezone.now)
+        status=models.CharField(max_length=10, choices=options, default='draft')
+
