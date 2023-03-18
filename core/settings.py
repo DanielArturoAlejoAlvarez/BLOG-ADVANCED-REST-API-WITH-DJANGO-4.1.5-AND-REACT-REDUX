b751a067b266c2a036deb6a2b158dc6fdfa68ac9
Daniel <yourmediasoft@gmail.com>
2023-03-18 16:25:04 -0500

    Modify settings file

--- core/settings.py
+++ core/settings.py
@@ -53,9 +53,6 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
-
-
-
 # Application definition
 
 #Default apps
