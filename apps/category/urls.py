0628958b68c94423ed24711360bb21815920ae1a
Daniel <yourmediasoft@gmail.com>
2023-03-20 14:24:54 -0500

    Creating a get_categories in category actions redux

--- apps/category/urls.py
+++ apps/category/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('categories', CategoryListView.as_view()),
 ]
---
+++ src/redux/actions/categories.js
@@ -0,0 +1,38 @@
+
+import axios from 'axios'
+import { 
+    GET_CATEGORIES_FAIL, 
+    GET_CATEGORIES_SUCCESS 
+} from 'redux/types/blog'
+
+
+export const get_categories = ()=> async (dispatch)=>{
+    const config = {
+        headers: {
+            Accept: "application/json"
+        }
+    }
+
+    try {
+        const resp = axios.get(`${process.env.REACT_APP_API_URL/api/category/categories}`, config)
+        if (resp.status === 200) {
+            dispatch({
+                type: GET_CATEGORIES_SUCCESS,
+                payload: resp.data
+            })
+        }else{
+            dispatch({
+                type: GET_CATEGORIES_FAIL,
+                payload: resp.data
+            }) 
+        }
+
+    } catch (error) {
+        console.log(error)
+        dispatch({
+            type: GET_CATEGORIES_FAIL,
+            payload: resp.data
+        }) 
+    }
+
+}
