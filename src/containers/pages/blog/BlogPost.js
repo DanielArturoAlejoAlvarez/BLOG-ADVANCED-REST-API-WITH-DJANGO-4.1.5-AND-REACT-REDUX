8118bba5ad13c2ec962203a3e19137c870d728ee
Daniel <yourmediasoft@gmail.com>
2023-03-24 18:29:33 -0500

    Creating BlogPost page

--- src/containers/pages/blog/BlogPost.js
+++ src/containers/pages/blog/BlogPost.js
@@ -1,31 +1,22 @@
-import FullWidthLayout from 'hocs/layouts/FullWidthLayout'
-import { useEffect } from 'react'
-import { connect } from 'react-redux'
-import { useParams } from 'react-router-dom'
-import { get_blog } from 'redux/actions/blog'
+import FullWidthLayout from "hocs/layouts/FullWidthLayout";
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
+import { get_blog } from "redux/actions/blog";
 
-function BlogPost({
-  get_blog
-}) {
-
-  const params = useParams()
-  const {slug} = params
+function BlogPost({ get_blog }) {
+  const params = useParams();
+  const { slug } = params;
 
   useEffect(() => {
-    get_blog(slug)
-  }, [])
-  
-  return (
-    <FullWidthLayout>
-        Blog Post
-    </FullWidthLayout>
-  )
+    get_blog(slug);
+  }, []);
+
+  return <FullWidthLayout>Blog Post</FullWidthLayout>;
 }
 
-const mapStateToProps = state=>({
-  
-})
+const mapStateToProps = (state) => ({});
 
 export default connect(mapStateToProps, {
   get_blog
-})(BlogPost)
+})(BlogPost);
