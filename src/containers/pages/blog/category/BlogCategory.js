9a988dcf8a3f8fbf84d155aa2623770f72531064
Daniel <yourmediasoft@gmail.com>
2023-03-25 19:26:07 -0500

    Creating BlogCategory page using get_blog_category_list function action redux

---
+++ src/containers/pages/blog/category/BlogCategory.js
@@ -0,0 +1,44 @@
+import BlogCategories from 'components/blog/BlogCategories'
+import FullWidthLayout from 'hocs/layouts/FullWidthLayout'
+import { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { get_blog_category_list, get_blog_category_list_page } from 'redux/actions/blog'
+
+function BlogCategory({
+    get_blog_category_list,
+    get_blog_category_list_page,
+    blog_list,
+    count
+}) {
+
+ const params = useParams()
+ const {category_id} = params
+
+ useEffect(() => {
+   get_blog_category_list(category_id)
+ }, [])
+ 
+
+
+  return (
+    <FullWidthLayout>
+        <BlogCategories 
+            get_blog_list_page={get_blog_category_list_page}
+            blog_list={blog_list}
+            count={count}
+            category_id={category_id}
+        />
+    </FullWidthLayout>
+  )
+}
+
+const mapStateToProps = state=>({
+    blog_list: state.blog.blog_category_list,
+    count: state.blog.count
+})
+
+export default connect(mapStateToProps, {
+    get_blog_category_list,
+    get_blog_category_list_page
+})(BlogCategory)
