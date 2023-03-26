2f90c45da2f458c32c33c952b54864f8fd907eb6
Daniel <yourmediasoft@gmail.com>
2023-03-25 23:05:43 -0500

    Config BlogPost page container

--- src/containers/pages/blog/BlogPost.js
+++ src/containers/pages/blog/BlogPost.js
@@ -1,10 +1,13 @@
+import BlogSkeletonLoader from "components/loaders/BlogSkeletonLoader";
 import FullWidthLayout from "hocs/layouts/FullWidthLayout";
 import { useEffect } from "react";
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
 import { get_blog } from "redux/actions/blog";
 
-function BlogPost({ get_blog }) {
+function BlogPost({ 
+  get_blog,
+  post }) {
   const params = useParams();
   const { slug } = params;
 
@@ -12,10 +15,49 @@ function BlogPost({ get_blog }) {
     get_blog(slug);
   }, []);
 
-  return <FullWidthLayout>Blog Post</FullWidthLayout>;
+  const base = process.env.REACT_APP_API_URL;
+
+  return (
+    <FullWidthLayout>
+      {
+        post ? (
+          <div className="relative py-16 bg-white overflow-hidden">
+          <div className="relative px-4 sm:px-6 lg:px-8">
+            <div className="text-lg max-w-prose mx-auto">
+              <div className="flex-shrink-0 shadow-md">
+                <img
+                  className="h-48 w-full object-cover"
+                  src={base + post.thumbnail}
+                  alt=""
+                />
+              </div>
+              <h1>
+                <span className="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
+                  {post.category.name}
+                </span>
+                <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                  {post.title}
+                </span>
+              </h1>
+              <p className="mt-8 text-xl text-gray-500 leading-8">
+                {post.excerpt}
+              </p>
+            </div>
+
+            <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+              <p>{post.content}</p>
+            </div>
+          </div>
+        </div>
+        ) : <BlogSkeletonLoader />
+      }
+    </FullWidthLayout>
+  )
 }
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  post: state.blog.post
+});
 
 export default connect(mapStateToProps, {
   get_blog
